863dbf42996c2e22d2beb53321be96547a5cca7d
Haruto Nishizuka <harutonishizuka0950@gmail.com>
2025-04-20 17:32:05 +0900

    [feat]コンポーネントのテスト

--- src/app.vue
+++ src/app.vue
@@ -1,6 +1,10 @@
+<script setup lang="ts">
+import Child from "./components/Child.vue";
+const name = ref("Nuxt");
+</script>
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <h1>Welcome to Nuxt</h1>
+    <Child :message="name" />
   </div>
 </template>
